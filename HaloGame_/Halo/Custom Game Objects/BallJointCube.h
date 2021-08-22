911a2a8a8e0588a97abf9e5139c4a6dec6ede890
yzd0014 <1051714925@qq.com>
2021-08-22 18:44:52 -0400

    Hing joint in reduced coordinate

--- HaloGame_/Halo/Custom Game Objects/BallJointCube.h
+++ HaloGame_/Halo/Custom Game Objects/BallJointCube.h
@@ -14,8 +14,10 @@ namespace eae6320
 		BallJointCube(Effect * i_pEffect, Assets::cHandle<Mesh> i_Mesh, Physics::sRigidBodyState i_State):
 			GameCommon::GameObject(i_pEffect, i_Mesh, i_State)
 		{
-			U_p = Vector3f(-1.0f, 1.0f, -1.0f);
-			m_State.position = -Math::sVector(U_p(0), U_p(1), U_p(2));
+			uJoinLocal = Vector3f(-1.0f, -1.0f, 0.0f);
+			uJointGlobal = uJoinLocal;
+			
+			m_State.position = -Math::sVector(uJointGlobal(0), uJointGlobal(1), uJointGlobal(2));
 			
 			M_d.resize(6, 6);
 			M_d.setZero();
@@ -30,38 +32,27 @@ namespace eae6320
 		}
 		void Tick(const float i_secondCountToIntegrate)
 		{
-			Vector3f gamma_theta = Vector3f(-o3_dot * sin(o3) * cos(o2) - o2_dot * cos(o3) * sin(o2), o2_dot * cos(o2), -o3_dot * cos(o3) * cos(o2) + o2_dot * sin(o3) * sin(o2)) * o1_dot 
-				+ Vector3f(cos(o3), 0.0f, -sin(o3)) * o3_dot * o2_dot;
-			//cout << gamma_theta << endl << endl;
-			
-			Vector3f v_x(cos(o3) * cos(o2), sin(o2), -sin(o3) * cos(o2));
-			Vector3f v_z(sin(o3), 0, cos(o3));
-			Vector3f v_y(0.0f, 1.0f, 0.0f);
-			Vector3f w = o1_dot * v_x + o2_dot * v_z + o3_dot * v_y;
+			Vector3f v_z(0, 0, 1);
+			Vector3f w = o_dot * v_z;
 
 			MatrixXf H_t;
-			H_t.resize(6, 3);
+			H_t.resize(6, 1);
 			H_t.setZero();
-			H_t.block<3, 1>(0, 0) = U_p.cross(v_x);
-			H_t.block<3, 1>(0, 1) = U_p.cross(v_z);
-			H_t.block<3, 1>(0, 2) = U_p.cross(v_y);
-			H_t.block<3, 1>(3, 0) = v_x;
-			H_t.block<3, 1>(3, 1) = v_z;
-			H_t.block<3, 1>(3, 2) = v_y;
+			H_t.block<3, 1>(0, 0) = uJointGlobal.cross(v_z);
+			H_t.block<3, 1>(3, 0) = v_z;
 
 			MatrixXf gamma_t;
 			gamma_t.resize(6, 1);
 			gamma_t.setZero();
-			gamma_t.block<3, 1>(0, 0) = U_p.cross(gamma_theta) - w.cross(w.cross(U_p));
-			gamma_t.block<3, 1>(3, 0) = gamma_theta; 
+			gamma_t.block<3, 1>(0, 0) =  - w.cross(w.cross(uJointGlobal));
 			//cout << gamma_t << endl << endl;
 
-			Matrix3f M_r = H_t.transpose() * M_d * H_t;
+			MatrixXf M_r = H_t.transpose() * M_d * H_t;
 
 			MatrixXf Fe;
 			Fe.resize(6, 1);
 			Fe.setZero();
-			//Fe.block<3, 1>(0, 0) = Vector3f(0.0f, 1.0f, 0.0f); //gravity
+			Fe.block<3, 1>(0, 0) = Vector3f(0.0f, -9.8f, 0.0f); //gravity
 			//cout << Fe << endl << endl;
 			MatrixXf Fv;
 			Fv.resize(6, 1);
@@ -69,47 +60,38 @@ namespace eae6320
 			Fv.block<3, 1>(3, 0) = -w.cross(globalInertiaTensor * w);
 			//cout << Fv << endl << endl;
 
-			Vector3f Q_r = H_t.transpose() * (Fe + Fv - M_d * gamma_t);
+			VectorXf Q_r = H_t.transpose() * (Fe + Fv - M_d * gamma_t);
 	
-			Vector3f Pr_ddot = M_r.inverse() * Q_r;
+			VectorXf Pr_ddot = M_r.inverse() * Q_r;
 			//integration
-			o1_dot = o1_dot + Pr_ddot(0) * i_secondCountToIntegrate;
-			o2_dot = o2_dot + Pr_ddot(1) * i_secondCountToIntegrate;
-			o3_dot = o3_dot + Pr_ddot(2) * i_secondCountToIntegrate;
-			o1 = o1 + o1_dot;
-			o2 = o2 + o2_dot;
-			o3 = o3 + o3_dot;
+			o_dot = o_dot + Pr_ddot(0) * i_secondCountToIntegrate;
+			o_dot *= 0.9f;//damping
+			o = o + o_dot;
 	
 			//update inertiaTensor and others
 			Matrix3f Rt;
-			Rt = AngleAxisf(o3, Vector3f::UnitY()) * AngleAxisf(o2, Vector3f::UnitZ()) * AngleAxisf(o1, Vector3f::UnitX());
+			Rt = AngleAxisf(o, Vector3f::UnitZ());
 			globalInertiaTensor = Rt * localInertiaTensor * Rt.inverse();
 			M_d.block<3, 3>(3, 3) = globalInertiaTensor;
-			U_p = Rt * Vector3f(-1.0f, 1.0f, -1.0f);
+			uJointGlobal = Rt * uJoinLocal;
 			
 			physicsStateUpdate();
  		}
 	private:
 		void physicsStateUpdate()
 		{
-			Math::cQuaternion q1 = Math::cQuaternion(o1, Math::sVector(1, 0, 0));
-			Math::cQuaternion q2 = Math::cQuaternion(o2, Math::sVector(0, 0, 1));
-			Math::cQuaternion q3 = Math::cQuaternion(o3, Math::sVector(0, 1, 0));
-			m_State.orientation = q3 * q2 * q1;
+			m_State.orientation = Math::cQuaternion(o, Math::sVector(0, 0, 1));
 			m_State.orientation.Normalize();
 
-			m_State.position = -Math::sVector(U_p(0), U_p(1), U_p(2));
+			m_State.position = -Math::sVector(uJointGlobal(0), uJointGlobal(1), uJointGlobal(2));
 		}
 
 	private:
-		float o1 = 0.0f;
-		float o2 = 0.0f;
-		float o3 = 0.0f;
-		float o1_dot = 0.0f;
-		float o2_dot = 0.0f;
-		float o3_dot = 0.0f;
+		float o = 0.0f;
+		float o_dot = 0.0f;
 		MatrixXf M_d;
-		Vector3f U_p;
+		Vector3f uJointGlobal;
+		Vector3f uJoinLocal;
 		Matrix3f localInertiaTensor;
 		Matrix3f globalInertiaTensor;
 	};
