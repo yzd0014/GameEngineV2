9f6fe88df33aa27a12d27c26a5241db24922638e
yzd0014 <1051714925@qq.com>
2024-06-23 01:15:42 -0700

    double zero twist vector added

--- HaloGame_/Halo/MyActor.cpp
+++ HaloGame_/Halo/MyActor.cpp
@@ -11,27 +11,46 @@
 eae6320::MyActor::MyActor(Effect * i_pEffect, Assets::cHandle<Mesh> i_Mesh, Physics::sRigidBodyState i_State):
 	GameCommon::GameObject(i_pEffect, i_Mesh, i_State)
 {
-	GameplayUtility::DrawArrow(Vector3d(0, 0, 0), Vector3d(1, 0, 0), Math::sVector(1, 0, 0), 0.1);
+	GameplayUtility::DrawArrow(Vector3d(0, 0, 0), Vector3d(0, 1, 0), Math::sVector(1, 0, 0), 1);
 	twistAxis0 = Vector3d(0, -1, 0);
+	twistAxis1 = Vector3d(0, 1, 0);
 	x0 = Vector3d(1, 0, 0);
 	z0 = Vector3d(0, 0, 1);
 }
 
 void eae6320::MyActor::Tick(const double i_secondCountToIntegrate)
 {
-	if (UserInput::KeyState::currFrameKeyState['F']) angleUpDown += 0.01;
-	if (UserInput::KeyState::currFrameKeyState['R']) angleLeftRight += 0.01;
+	if (UserInput::KeyState::currFrameKeyState['F']) angleUpDown += i_secondCountToIntegrate;
+	if (UserInput::KeyState::currFrameKeyState['R']) angleLeftRight += i_secondCountToIntegrate;
 	if (angleUpDown > 2 * M_PI) angleUpDown = 0;
 	if (angleLeftRight > 2 * M_PI) angleLeftRight = 0;
 
 	Vector3d twistAxisWorld;
+	Matrix3d Ry;
+	Ry = Math::RotationConversion_VecToMatrix(Vector3d(0, angleLeftRight, 0));
+
+	Vector3d r0;
+	r0 = Vector3d(-angleUpDown, 0, 0);
+	r0 = Ry * r0;
+	Vector3d r1;
+	r1= Vector3d(M_PI - angleUpDown, 0, 0);
+	r1 = Ry * r1;
 	
-	Vector3d r(-1, 0, 0);
-	r = angleUpDown * r;
-	Matrix3d Ry = Math::RotationConversion_VecToMatrix(Vector3d(0, angleLeftRight, 0));
-	r = Ry * r;
-	Matrix3d R = Math::RotationConversion_VecToMatrix(r);
-	twistAxisWorld = R * twistAxis0;
+	Matrix3d R0 = Math::RotationConversion_VecToMatrix(r0);
+	Matrix3d R1 = Math::RotationConversion_VecToMatrix(r1);
+	
+	Matrix3d R;
+	if (angleUpDown < 0.5 * M_PI || angleUpDown > 1.5 * M_PI)
+	{
+		R = R0;
+		twistAxisWorld = R * twistAxis0;
+	}
+	else
+	{
+		R = R1;
+		twistAxisWorld = R * twistAxis1;
+	}
+
 	Vector3d x0World = R * x0;
 	Vector3d z0World = R * z0;
 	
