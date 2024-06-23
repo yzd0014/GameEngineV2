0ff4c32865c549a68488fcd5ed60e486083fb8f3
yzd0014 <1051714925@qq.com>
2024-06-23 01:33:52 -0700

    static view of zero twist

--- HaloGame_/Halo/MyActor.cpp
+++ HaloGame_/Halo/MyActor.cpp
@@ -11,16 +11,36 @@
 eae6320::MyActor::MyActor(Effect * i_pEffect, Assets::cHandle<Mesh> i_Mesh, Physics::sRigidBodyState i_State):
 	GameCommon::GameObject(i_pEffect, i_Mesh, i_State)
 {
-	GameplayUtility::DrawArrow(Vector3d(0, 0, 0), Vector3d(0, 1, 0), Math::sVector(1, 0, 0), 1);
+	GameplayUtility::DrawArrow(Vector3d(0, 0, 0), Vector3d(0, -1, 0), Math::sVector(1, 0, 0), 0.9);
 	twistAxis0 = Vector3d(0, -1, 0);
 	twistAxis1 = Vector3d(0, 1, 0);
 	x0 = Vector3d(1, 0, 0);
 	z0 = Vector3d(0, 0, 1);
+
+	Vector3d twistAxisWorld;
+	Matrix3d Ry;
+	for (angleLeftRight = 0; angleLeftRight <= M_PI; angleLeftRight += M_PI * 0.1)
+	{
+		Ry = Math::RotationConversion_VecToMatrix(Vector3d(0, angleLeftRight, 0));
+		for (angleUpDown = 0; angleUpDown <= 2 * M_PI; angleUpDown += M_PI * 0.1)
+		{
+			Vector3d r0;
+			r0 = Vector3d(-angleUpDown, 0, 0);
+			r0 = Ry * r0;
+			Matrix3d R = Math::RotationConversion_VecToMatrix(r0);
+			twistAxisWorld = R * twistAxis0;
+			Vector3d x0World = R * x0;
+			Vector3d z0World = R * z0;
+			GameplayUtility::DrawArrow(twistAxisWorld * rigidBodyScale, x0World, Math::sVector(1, 0, 0), 0.05);
+			GameplayUtility::DrawArrow(twistAxisWorld * rigidBodyScale, z0World, Math::sVector(0, 0, 1), 0.05);
+		}
+	}
+	
 }
 
 void eae6320::MyActor::Tick(const double i_secondCountToIntegrate)
 {
-	if (UserInput::KeyState::currFrameKeyState['F']) angleUpDown += i_secondCountToIntegrate;
+	/*if (UserInput::KeyState::currFrameKeyState['F']) angleUpDown += i_secondCountToIntegrate;
 	if (UserInput::KeyState::currFrameKeyState['R']) angleLeftRight += i_secondCountToIntegrate;
 	if (angleUpDown > 2 * M_PI) angleUpDown = 0;
 	if (angleLeftRight > 2 * M_PI) angleLeftRight = 0;
@@ -65,6 +85,6 @@ void eae6320::MyActor::Tick(const double i_secondCountToIntegrate)
 		zArrow = nullptr;
 	}
 	xArrow = GameplayUtility::DrawArrow(twistAxisWorld * rigidBodyScale, x0World, Math::sVector(1, 0, 0), 0.5);
-	zArrow = GameplayUtility::DrawArrow(twistAxisWorld * rigidBodyScale, z0World, Math::sVector(0, 0, 1), 0.5);
+	zArrow = GameplayUtility::DrawArrow(twistAxisWorld * rigidBodyScale, z0World, Math::sVector(0, 0, 1), 0.5);*/
 }
 
